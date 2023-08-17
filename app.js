71aa0fa177633f248303468991e61da5ae0ea291
hulashc <40060031+hulashc@users.noreply.github.com>
2023-08-18 00:35:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,86 +1,96 @@
+// Update your import to include the correct ABI definitions
+const { auctionFactoryABI, englishAuctionABI } = require('./abis');
+
 window.addEventListener('load', async () => {
-  if (typeof web3 !== 'undefined') {
-      web3 = new Web3(web3.currentProvider);
-  } else {
-      console.log('No web3 provider detected. Please install MetaMask or use an Ethereum-enabled browser.');
-      return;
-  }
+    if (typeof window.ethereum !== 'undefined') {
+        window.web3 = new Web3(window.ethereum);
+    } else {
+        console.log('No Ethereum provider detected. Please install MetaMask or use an Ethereum-enabled browser.');
+        return;
+    }
 
-  const factoryAddress = '0xA284243134358C3F21C649918Baf5c040BEeaf61'; // Replace with the actual Factory contract address
-  const factoryContract = new web3.eth.Contract(auctionFactoryABI, factoryAddress);
+    try {
+        await window.ethereum.enable();
+    } catch (error) {
+        console.error('Error enabling Ethereum provider:', error);
+        return;
+    }
 
-  let currentAccount;
+    const factoryAddress = '0xA284243134358C3F21C649918Baf5c040BEeaf61'; // Replace with the actual Factory contract address
+    const factoryContract = new window.web3.eth.Contract(auctionFactoryABI, factoryAddress);
 
-  try {
-      const accounts = await web3.eth.getAccounts();
-      currentAccount = accounts[0];
-  } catch (error) {
-      console.error('Error getting accounts:', error);
-  }
+    let currentAccount;
 
-  if (!currentAccount) {
-      console.log('No account detected. Please log in to MetaMask.');
-      return;
-  }
+    try {
+        const accounts = await window.web3.eth.getAccounts();
+        currentAccount = accounts[0];
+    } catch (error) {
+        console.error('Error getting accounts:', error);
+    }
 
-  const auctionList = document.getElementById('auctionList');
+    if (!currentAccount) {
+        console.log('No account detected. Please log in to MetaMask.');
+        return;
+    }
 
-  async function displayAuctions() {
-      auctionList.innerHTML = ''; // Clear previous list
-      const auctions = await factoryContract.methods.getAuctions().call();
+    const auctionList = document.getElementById('auctionList');
 
-      auctions.forEach(async (auctionAddress) => {
-          const auctionContract = new web3.eth.Contract(englishAuctionABI, auctionAddress);
-          const details = await auctionContract.methods.getAuctionDetails().call();
+    async function displayAuctions() {
+        auctionList.innerHTML = ''; // Clear previous list
+        const auctions = await factoryContract.methods.getAuctions().call();
 
-          const listItem = document.createElement('li');
-          listItem.textContent = `Auction ID: ${auctionAddress}, NFT Contract: ${details.nftContract}`;
-          listItem.addEventListener('click', () => displayAuctionDetails(auctionAddress));
-          auctionList.appendChild(listItem);
-      });
-  }
+        auctions.forEach(async (auctionAddress) => {
+            const auctionContract = new window.web3.eth.Contract(englishAuctionABI, auctionAddress);
+            const details = await auctionContract.methods.getAuctionDetails().call();
 
-  const auctionDetails = document.getElementById('auctionDetails');
-  const nftContract = document.getElementById('nftContract');
-  const nftIdDetail = document.getElementById('nftIdDetail');
-  const startingBidDetail = document.getElementById('startingBidDetail');
-  const placeBidForm = document.getElementById('placeBidForm');
-  const bidAmountInput = document.getElementById('bidAmount');
+            const listItem = document.createElement('li');
+            listItem.textContent = `Auction ID: ${auctionAddress}, NFT Contract: ${details.nftContract}`;
+            listItem.addEventListener('click', () => displayAuctionDetails(auctionAddress));
+            auctionList.appendChild(listItem);
+        });
+    }
 
-  async function displayAuctionDetails(auctionAddress) {
-      const auctionContract = new web3.eth.Contract(englishAuctionABI, auctionAddress);
-      const details = await auctionContract.methods.getAuctionDetails().call();
+    const auctionDetails = document.getElementById('auctionDetails');
+    const nftContract = document.getElementById('nftContract');
+    const nftIdDetail = document.getElementById('nftIdDetail');
+    const startingBidDetail = document.getElementById('startingBidDetail');
+    const placeBidForm = document.getElementById('placeBidForm');
+    const bidAmountInput = document.getElementById('bidAmount');
 
-      nftContract.textContent = details.nftContract;
-      nftIdDetail.textContent = details.nftId;
-      startingBidDetail.textContent = details.startingBid;
+    async function displayAuctionDetails(auctionAddress) {
+        const auctionContract = new window.web3.eth.Contract(englishAuctionABI, auctionAddress);
+        const details = await auctionContract.methods.getAuctionDetails().call();
 
-      auctionDetails.style.display = 'block';
-  }
+        nftContract.textContent = details.nftContract;
+        nftIdDetail.textContent = details.nftId;
+        startingBidDetail.textContent = details.startingBid;
 
-  placeBidForm.addEventListener('submit', async (event) => {
-      event.preventDefault();
-      const bidAmount = bidAmountInput.value;
-      const auctionAddress = auctionDetails.querySelector('span').textContent;
-      const auctionContract = new web3.eth.Contract(englishAuctionABI, auctionAddress);
+        auctionDetails.style.display = 'block';
+    }
 
-      try {
-          await auctionContract.methods.bid().send({
-              from: currentAccount,
-              value: web3.utils.toWei(bidAmount, 'ether')
-          });
-          console.log('Bid placed successfully.');
-          displayAuctionDetails(auctionAddress);
-          displayAuctions();
-      } catch (error) {
-          console.error('Error placing bid:', error);
-      }
-  });
+    placeBidForm.addEventListener('submit', async (event) => {
+        event.preventDefault();
+        const bidAmount = bidAmountInput.value;
+        const auctionAddress = auctionDetails.querySelector('span').textContent;
+        const auctionContract = new window.web3.eth.Contract(englishAuctionABI, auctionAddress);
 
-  const updateInterval = 5000; // Update every 5 seconds
-  setInterval(() => {
-      displayAuctions();
-  }, updateInterval);
+        try {
+            await auctionContract.methods.bid().send({
+                from: currentAccount,
+                value: window.web3.utils.toWei(bidAmount, 'ether')
+            });
+            console.log('Bid placed successfully.');
+            displayAuctionDetails(auctionAddress);
+            displayAuctions();
+        } catch (error) {
+            console.error('Error placing bid:', error);
+        }
+    });
 
-  displayAuctions();
+    const updateInterval = 5000; // Update every 5 seconds
+    setInterval(() => {
+        displayAuctions();
+    }, updateInterval);
+
+    displayAuctions();
 });
